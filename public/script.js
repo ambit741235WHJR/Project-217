6bf2fcb1aa4716ce85906e067e57ec0d074812e0
Ambit Biswas <ambit.biswas.whjr@gmail.com>
2024-06-07 21:27:12 +0530

    Fourth Commit for Project 217

--- public/script.js
+++ public/script.js
@@ -1,24 +1,26 @@
 const socket = io("/");
+user = prompt("Enter your name: ");
 
 $(function () {
     $("#send").click(function () {
         if ($("#chat_message").val().length !== 0) {
-            console.log(user);
-            socket.emit("message", $("#chat_message").val());
+            //console.log(user);
+            socket.emit("message", $("#chat_message").val(), user);
             $("#chat_message").val("");
         }
     })
     $("#chat_message").keydown(function (e) {
         if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
-            socket.emit("message", $("#chat_message").val());
+            socket.emit("message", $("#chat_message").val(), user);
             $("#chat_message").val("");
         }
     })
 })
 
-socket.on("createMessage", (message) => {
+socket.on("createMessage", (message, userName) => {
     $(".messages").append(`
         <div class="message">
+            <b><span class="username">${userName}: </span></b>
             <span>${message}</span>
         </div>
     `)
